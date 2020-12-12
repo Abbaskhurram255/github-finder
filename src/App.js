235f6111da6eff4b4b0b2a754c3aa5c36ae5d5ac
Alex <39873704+Abbaskhurram255@users.noreply.github.com>
2020-12-12 16:36:26 +0500

    Init

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import User from './components/users/User';
+import Alert from './components/layout/Alert';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
+import About from './components/pages/About';
+
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+
+import './App.css';
+
+const App = () => {
+  const [alert, setAlert] = useState(null);
+
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar title="Github Finder" icon="fa fa-github" />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  );
+};
+
+export default App;
